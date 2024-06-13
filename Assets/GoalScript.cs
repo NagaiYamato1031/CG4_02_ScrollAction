4a599e1b820817258a9f9b6fa5570361f46aab0e
NagaiYamato <115054936+NagaiYamato1031@users.noreply.github.com>
2024-06-13 11:49:45 +0900

    ゲームループを修正

--- Assets/GoalScript.cs
+++ Assets/GoalScript.cs
@@ -10,13 +10,13 @@ public class GoalScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        isGameClear = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
