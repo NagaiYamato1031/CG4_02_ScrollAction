3ccf6399024ef17f86d24256ee03cd3441574f4b
NagaiYamato <115054936+NagaiYamato1031@users.noreply.github.com>
2024-06-16 22:02:57 +0900

    ボタンに対応

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -6,72 +6,72 @@ using TMPro;
 
 public class GameManagerScript : MonoBehaviour
 {
-    public GameObject block;
-    public GameObject block2;
-    public GameObject goal;
-    public GameObject coin;
-    public TextMeshProUGUI scoreText;
+	public GameObject block;
+	public GameObject block2;
+	public GameObject goal;
+	public GameObject coin;
+	public TextMeshProUGUI scoreText;
 
-    // スコア
-    public static int score = 0;
+	// スコア
+	public static int score = 0;
 
-    int[,] map =
-    {
-        {1,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,1 },
-        {1,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,1 },
-        {1,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,3,0,0,1 },
-        {1,0,0,0,0,0,0,0,0,0, 0,0,0,3,0,0,0,0,3,0, 0,0,0,0,0,0,0,0,0,0, 0,0,3,0,0,0,0,0,0,1 },
-        {1,0,0,0,0,0,0,0,0,0, 0,1,0,0,0,0,0,0,0,0, 0,0,0,0,3,0,0,0,0,0, 0,1,1,1,1,0,0,0,0,1 },
-        {1,0,0,0,0,0,0,0,0,3, 0,1,1,0,0,0,0,1,1,1, 0,0,1,1,1,1,1,0,0,0, 0,0,0,0,0,0,0,0,2,1 },
-        {1,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,3,0,0,0,0, 0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,1,1,1,1 },
-        {1,0,0,0,0,1,1,0,0,0, 0,0,0,0,1,1,0,0,0,0, 0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,1,1,1,1,1 },
-        {1,0,0,3,1,1,1,0,0,0, 3,0,0,1,1,1,1,1,0,0, 0,0,0,0,0,0,0,0,1,1, 0,0,0,0,1,1,1,1,1,1 },
-        {1,1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1,1 }
-    };
+	int[,] map =
+	{
+		{1,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,1 },
+		{1,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,1 },
+		{1,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,3,0,0,1 },
+		{1,0,0,0,0,0,0,0,0,0, 0,0,0,3,0,0,0,0,3,0, 0,0,0,0,0,0,0,0,0,0, 0,0,3,0,0,0,0,0,0,1 },
+		{1,0,0,0,0,0,0,0,0,0, 0,1,0,0,0,0,0,0,0,0, 0,0,0,0,3,0,0,0,0,0, 0,1,1,1,1,0,0,0,0,1 },
+		{1,0,0,0,0,0,0,0,0,3, 0,1,1,0,0,0,0,1,1,1, 0,0,1,1,1,1,1,0,0,0, 0,0,0,0,0,0,0,0,2,1 },
+		{1,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,3,0,0,0,0, 0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,1,1,1,1 },
+		{1,0,0,0,0,1,1,0,0,0, 0,0,0,0,1,1,0,0,0,0, 0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,1,1,1,1,1 },
+		{1,0,0,3,1,1,1,0,0,0, 3,0,0,1,1,1,1,1,0,0, 0,0,0,0,0,0,0,0,1,1, 0,0,0,0,1,1,1,1,1,1 },
+		{1,1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1,1 }
+	};
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        // フルHD
-        Screen.SetResolution(1920, 1080, false);
+	// Start is called before the first frame update
+	void Start()
+	{
+		// フルHD
+		Screen.SetResolution(1920, 1080, false);
 
 
-        Vector3 position;
-        for (int y = 0; y < map.GetLength(0); y++)
-        {
-            for (int x = 0; x < map.GetLength(1); x++)
-            {
-                position = new Vector3(x - 1, -y + map.GetLength(0));
-                if (map[y, x] == 1)
-                {
-                    Instantiate(block, position, Quaternion.identity);
-                }
-                else if (map[y, x] == 2)
-                {
-                    goal.transform.position = position;
-                }
-                else if (map[y, x] == 3)
-                {
-                    Instantiate(coin, position, Quaternion.identity);
-                }
-                position.z += 1.0f;
-                Instantiate(block2, position, Quaternion.identity);
-            }
-        }
+		Vector3 position;
+		for (int y = 0; y < map.GetLength(0); y++)
+		{
+			for (int x = 0; x < map.GetLength(1); x++)
+			{
+				position = new Vector3(x - 1, -y + map.GetLength(0));
+				if (map[y, x] == 1)
+				{
+					Instantiate(block, position, Quaternion.identity);
+				}
+				else if (map[y, x] == 2)
+				{
+					goal.transform.position = position;
+				}
+				else if (map[y, x] == 3)
+				{
+					Instantiate(coin, position, Quaternion.identity);
+				}
+				position.z += 1.0f;
+				Instantiate(block2, position, Quaternion.identity);
+			}
+		}
 
-        score = 0;
-    }
+		score = 0;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (GoalScript.isGameClear)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                SceneManager.LoadScene("TitleScene");
-            }
-        }
-        scoreText.text = "Score : " + score;
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		if (GoalScript.isGameClear)
+		{
+			if (Input.GetButtonDown("Jump"))
+			{
+				SceneManager.LoadScene("TitleScene");
+			}
+		}
+		scoreText.text = "Score : " + score;
+	}
 }
--- Assets/TitleScript.cs
+++ Assets/TitleScript.cs
@@ -20,7 +20,7 @@ public class TitleScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetButtonDown("Jump"))
         {
             SceneManager.LoadScene(nextSceneName);
         }
